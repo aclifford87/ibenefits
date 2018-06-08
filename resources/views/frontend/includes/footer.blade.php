90a3e2f981a05e7f6bd53b3c426f296e55f4ee75
Manvir Sandhu <manvir.sandhu@growthpartnersplc.co.uk>
2018-06-08 16:02:33 +0100

    Footer - Change 1

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,5 +1,29 @@
 <!-- FOOTER -->
 <footer class="container">
+
+    <div class="card-group">
+        <div class="card">
+            <div class="card-body">
+                <p class="card-title">Customer service</p>
+
+                <p class="card-text">Help Center</p>
+                <p class="card-text">Help Center</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <p class="card-title">Card title</p>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <p class="card-title">Card title</p>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+        </div>
+    </div>
+
     <p class="float-right"><a href="#">Back to top</a></p>
     <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 </footer>
