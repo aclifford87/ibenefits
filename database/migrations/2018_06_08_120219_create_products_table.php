8300b90b9579ece95e6cfa35715ed55f1aa1720e
Nick Ashford <N.Ashford@Internal.growthpartnersplc.co.uk>
2018-06-08 13:12:58 +0100

    Added Products migration

--- database/migrations/2018_06_08_120219_create_products_table.php
+++ database/migrations/2018_06_08_120219_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('description');
+            $table->string('image_url');
+            $table->float('price');
+            $table->boolean('visible');
             $table->timestamps();
         });
     }
