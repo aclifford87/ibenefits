2a1488098dbd9b92d4119b24a90df676bc61dcb0
Manvir Sandhu <manvir.sandhu@growthpartnersplc.co.uk>
2018-06-08 16:58:34 +0100

    Footer - changes 3

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,5 +1,34 @@
 <!-- FOOTER -->
 <footer class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <h6>Customer service</h6>
+
+            <p>Help Center</p>
+            <p>Contact Us</p>
+            <p>Report</p>
+            <p>Abuse</p>
+        </div>
+
+        <div class="col-md-3">
+            <h6>About us</h6>
+            <p>About ibenefits.com</p>
+            <p>About Growth Partners</p>
+            <p>sitemap</p>
+        </div>
+
+        <div class="col-md-3">
+            <h6>Buy on ibenefits.com</h6>
+
+            <p>All Categories Request for</p>
+            <p>Quotation Wholesaler Market</p>
+        </div>
+
+        <div class="col-md-3">
+            <h6>test4</h6>
+        </div>
+    </div>
+
     <p class="float-right"><a href="#">Back to top</a></p>
     <p>&copy; 2017-2018 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 </footer>
