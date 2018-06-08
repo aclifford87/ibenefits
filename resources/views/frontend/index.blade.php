268ff64c5a530b18111c97560c0e02e98d25dc03
Manvir Sandhu <manvir.sandhu@growthpartnersplc.co.uk>
2018-06-08 10:16:25 +0100

    Section Removed

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,42 +3,6 @@
 @section('title', app_name() . ' | '.__('navs.general.home'))
 
 @section('content')
-<<<<<<< HEAD
-    <div class="row mb-4">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fas fa-home"></i> {{ __('navs.general.home') }}
-                </div>
-                <div class="card-body">
-                    {{--{{ __('strings.frontend.welcome_to', ['place' => app_name()]) }}--}}
-                </div>
-            </div><!--card-->
-        </div><!--col-->
-    </div><!--row-->
-
-    <div class="row mb-4">
-        <div class="col">
-            <example-component></example-component>
-        </div><!--col-->
-    </div><!--row-->
-
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">
-                    <i class="fab fa-font-awesome-flag"></i> Font Awesome {{ __('strings.frontend.test') }}
-                </div>
-                <div class="card-body">
-                    <i class="fas fa-home"></i>
-                    <i class="fab fa-facebook"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-pinterest"></i>
-                </div><!--card-body-->
-            </div><!--card-->
-        </div><!--col-->
-    </div><!--row-->
-=======
     <main role="main">
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
