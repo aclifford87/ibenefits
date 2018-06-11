07e90eea1d0d40e9c633e17a7748e65ea816945d
Manvir Sandhu <manvir.sandhu@growthpartnersplc.co.uk>
2018-06-11 16:41:25 +0100

    Index and Footer page - layout changes

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,8 +1,7 @@
 <!-- FOOTER -->
-<footer class="container pt-5" >
-    <div class="p-3 mb-2 bg-light text-dark">
+<footer class="container pt-5 w-100" >
+    <div class="w-auto p-3 mb-2 bg-light text-dark font-weight-light col-lg-auto">
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
-
 
     <div class="row pt-3">
         <div class="col-md-3">
@@ -26,7 +25,7 @@
         </div>
     </div>
 
-    <p class="float-right"> <i class="fab fa-facebook-f"></i> <i class="fab fa-twitter"></i> <i class="fab fa-instagram"></i></p>
-    <p>&copy; Copyright 2018 Growth Partners PLC &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a> &middot; <a href="#">Back to top</a></p>
+    <p class="float-right pr-5 pt-5"> <i class="fab fa-facebook-f fa-lg"></i> <i class="fab fa-twitter fa-lg w-5"></i> <i class="fab fa-instagram fa-lg w-5"></i></p>
+    <p class="copyright pt-5"> &copy; Copyright 2018 Growth Partners PLC &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a> &middot; <a href="#">Back to top</a></p>
     </div>
 </footer>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,20 +4,20 @@
 
 @section('content')
     <main role="main">
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div id="myCarousel" class="carousel slide h-25" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                 <li data-target="#myCarousel" data-slide-to="1"></li>
                 <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
                     <div class="container">
-                        <div class="carousel-caption text-left">
-                            <h1>Example headline.</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                        <div class="carousel-caption">
+                            <h1>INSURANCE & POSITIVE LIFESTYLE REWARDS</h1>
+                            <p>Insert image</p>
                         </div>
                     </div>
                 </div>
@@ -25,19 +25,26 @@
                     <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
                     <div class="container">
                         <div class="carousel-caption">
-                            <h1>Another example headline.</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                            <h1>CHILDCARE VOUCHERS</h1>
+                            <p>Insert image</p>
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
                     <div class="container">
-                        <div class="carousel-caption text-right">
-                            <h1>One more for good measure.</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                        <div class="carousel-caption">
+                            <h1>ONLINE TRAINING COURSES</h1>
+                            <p>Insert image</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="fourth-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Fourth slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1>PERSONAL DEVELOPMENT</h1>
+                            <p>Insert image</p>
                         </div>
                     </div>
                 </div>
@@ -56,7 +63,7 @@
         ================================================== -->
         <!-- Wrap the rest of the page in another container to center all the content. -->
 
-        <div class="container marketing">
+        <div class="container marketing font-weight-light">
 
             <!-- Three columns of text below the carousel -->
             <div class="row">
