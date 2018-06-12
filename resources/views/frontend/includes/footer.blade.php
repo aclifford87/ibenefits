521826c8ac2c709f8cf9647eeb28ccfc3ac57e3c
Manvir Sandhu <manvir.sandhu@growthpartnersplc.co.uk>
2018-06-12 14:22:00 +0100

    Footer page - padding changes

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,31 +1,33 @@
 <!-- FOOTER -->
-<footer class="container pt-5 w-100" >
+<footer class="pt-5 pb-0 w-100" >
     <div class="w-auto p-3 mb-2 bg-light text-dark font-weight-light col-lg-auto">
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
-
-    <div class="row pt-3">
-        <div class="col-md-3">
+        <div class="row pt-3 offset-xl-1 offset-md-1 pl-3 pr-lg-3">
+            <div class="col-md-3">
             <h6><u>Customer service</u></h6></br>
             <p>Help Center</br>Contact Us</br>Report</br>Abuse</p>
-        </div>
+            </div>
 
-        <div class="col-md-3">
+            <div class="col-md-3">
             <h6><u>About us</u></h6></br>
             <p>About ibenefits.com</br>About Growth Partners</br>Sitemap</p>
-        </div>
+            </div>
 
-        <div class="col-md-3">
+            <div class="col-md-3">
             <h6><u>Buy on ibenefits.com</u></h6></br>
             <p>All Categories Request for</br>Quotation Wholesaler Market</p>
-        </div>
+            </div>
 
-        <div class="col-md-3">
+            <div class="col-md-3">
             <h6></h6></br>
             <p>Submit a Dispute</br>Policies & Rules Get</br>Paid for Your Feedback</p>
-        </div>
-    </div>
+            </div>
 
-    <p class="float-right pr-5 pt-5"> <i class="fab fa-facebook-f fa-lg"></i> <i class="fab fa-twitter fa-lg w-5"></i> <i class="fab fa-instagram fa-lg w-5"></i></p>
-    <p class="copyright pt-5"> &copy; Copyright 2018 Growth Partners PLC &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a> &middot; <a href="#">Back to top</a></p>
+            <div class="row col-xl-12">
+                <div class="col-8">&copy; Copyright 2018 Growth Partners PLC &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a> &middot; <a href="#">Back to top</a></div>
+                <div class="col-4 pl-lg-3 float-right"><i class="fab fa-facebook-f fa-lg"></i> <i class="fab fa-twitter fa-lg w-5"></i> <i class="fab fa-instagram fa-lg w-5"></i></div>
+            </div>
+
+        </div>
     </div>
 </footer>
